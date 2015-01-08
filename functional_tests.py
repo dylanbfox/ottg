19fcb67999072a590a0fc351c6d2edd9ea30a098
Dylan Fox <dylan@crowdvance.com>
2015-01-08 18:47:55 -0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 # Tests are organized into classes, which inherit 
@@ -25,10 +26,26 @@ class NewVisitorTest(unittest.TestCase):
 
 	# nice descriptive names are a good idea
 	def test_can_start_a_list_and_retrieve_it_later(self):
-
 		self.browser.get('http://localhost:8000')
 		# special assert function provided by unittest
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
 		# fails no matter what, producing the error message given.
 		# using it as a reminder here to finish the tests!
 		self.fail('Finish the test!')
