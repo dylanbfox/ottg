ab3cd903837300620fc4b1f22c6afdd9752484ed
Dylan Fox <dylan@crowdvance.com>
2015-01-08 17:10:00 -0500

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+# Tests are organized into classes, which inherit 
+# from unittest.TestCase 
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	# special method; gets called before each test
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		# browser will wait 3 seconds before trying anything
+		# to help guarantee everything has loaded before
+		# continuing to test
+		self.browser.implicity_wait(3)
+
+	# special method; gets called after each test
+	# even if there is an error in the test
+	def tearDown(self):
+		self.browser.quit()
+
+	# any method whos name starts with test_ is a test
+	# method and will be run by the test runner
+
+	# you can have more than one test_ method per class
+
+	# nice descriptive names are a good idea
+	def test_can_start_a_list_and_retrieve_it_later(self):
+
+		self.browser.get('http://localhost:8000')
+		# special assert function provided by unittest
+		self.assertIn('To-Do', self.browser.title)
+		# fails no matter what, producing the error message given.
+		# using it as a reminder here to finish the tests!
+		self.fail('Finish the test!')
+
+# This is how a Python script checks if it's been
+# executed from the command line, rather than just imported
+# by another script
+if __name__ == '__main__':
+	# launches the unittest test runner, which will
+	# automatically find test classes and methods in the file
+	# and run them
+	unittest.main()
