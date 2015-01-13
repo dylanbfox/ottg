ed781d123b7a3bccb721344b3edc4c1e1dbe86a1
Dylan Fox <dylan@crowdvance.com>
2015-01-13 14:50:12 -0500

    urls for forms x2, moved code in views + tests, new URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -45,7 +45,13 @@ class NewVisitorTest(LiveServerTestCase):
 		)
 
 		inputbox.send_keys('Buy peacock feathers')
+
+		# when she hits enter, she is taken to a new URL
+		# and now the page lists "1: Buy peacock feathers" as an item in a
+		# to- do list table
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegexpMatches(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still an input box inviting user to add another item.
@@ -57,6 +63,35 @@ class NewVisitorTest(LiveServerTestCase):
 		# the page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+		# Now a new user, Francis, comes along to the site
+
+		## we use a new browser session to make sure that no information
+		## of Edith's is coming through from cookies, etc.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# Francis visit the home page. There is no sign of Edith's list
+		self.browser.get(self.live_server_url) # go to server URL (now that we're using LiveServerTestCase)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text) 
+
+		# Francis starts a new list by entering a new item. He is less
+		# interesting than Edith...
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegexpMatches(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 
 		# fails no matter what, producing the error message given.
 		# using it as a reminder here to finish the tests!
