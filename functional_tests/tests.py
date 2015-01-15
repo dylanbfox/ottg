a5234bd2eaef6431056a42f49d4c02e9115311f3
Dylan Fox <dylan@crowdvance.com>
2015-01-15 13:25:17 -0500

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,29 @@
+import sys
+import unittest
+
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 # Tests are organized into classes, which inherit 
 # from unittest.TestCase 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super(NewVisitorTest, cls).setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super(NewVisitorTest, cls).tearDownClass()
 
 	# special method; gets called before each test
 	def setUp(self):
@@ -25,7 +41,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		# She notices the input box is nicely centered
@@ -58,7 +74,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	# nice descriptive names are a good idea
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		# special assert function provided by unittest
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -97,7 +113,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		# Francis visit the home page. There is no sign of Edith's list
-		self.browser.get(self.live_server_url) # go to server URL (now that we're using LiveServerTestCase)
+		self.browser.get(self.server_url) # go to server URL (now that we're using LiveServerTestCase)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text) 
