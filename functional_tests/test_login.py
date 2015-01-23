82ef0da72096ee33f9b6803b84823a2f889e8e9c
Dylan Fox <dylan@crowdvance.com>
2015-01-23 12:46:42 -0500

    Housekeeping

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,3 +1,5 @@
+import time
+
 from .base import FunctionalTest
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -23,7 +25,7 @@ class LoginTest(FunctionalTest):
 	def test_login_with_persona(self):
 		# Edith clicks the sign in link
 		self.browser.get(self.server_url)
-		self.browser.find_element_by_id('login').click()
+		self.browser.find_element_by_id('id_login').click()
 
 		# A persona login box appears
 		self.switch_to_new_window('Mozilla Persona')
@@ -38,6 +40,6 @@ class LoginTest(FunctionalTest):
 		self.switch_to_new_window('To-Do')
 
 		# She can now see that she is logged in
-		self.wait_for_element_with_id('logout')
+		self.wait_for_element_with_id('id_logout')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertIn('edith@mockmyid.com', navbar.text)
