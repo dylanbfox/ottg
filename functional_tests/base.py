1cf5e696f1ec87e301e075c382663742df573f6e
Dylan Fox <dylan@crowdvance.com>
2015-01-30 16:03:23 -0500

    voodoo

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,6 +2,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from .server_tools import reset_database
+
 import sys
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -10,18 +12,23 @@ class FunctionalTest(StaticLiveServerTestCase):
 	def setUpClass(cls):
 		for arg in sys.argv:
 			if 'liveserver' in arg:
+				cls.server_host = arg.split('=')[1]
 				cls.server_url = 'http://' + arg.split('=')[1]
+				cls.against_staging = True
 				return
 		super(FunctionalTest, cls).setUpClass()
 		cls.server_url = cls.live_server_url
+		cls.against_staging = False
 
 	@classmethod
 	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
+		if not cls.against_staging:
 			super(FunctionalTest, cls).tearDownClass()
 
 	# special method; gets called before each test
 	def setUp(self):
+		if self.against_staging:
+			reset_database(self.server_host)
 		self.browser = webdriver.Firefox()
 		# browser will wait 3 seconds before trying anything
 		# to help guarantee everything has loaded before
